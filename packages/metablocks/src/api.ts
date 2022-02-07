f382311a6f798ff7ed6605eb859ff22653ed49f6
valekar <srinivas.valekar@gmail.com>
2022-02-07 21:21:43 +0530

    fix: remove intermediate APIs of deposit nft

--- packages/metablocks/src/api.ts
+++ packages/metablocks/src/api.ts
@@ -5,21 +5,11 @@ import {
   computeUpdateUniverseParams,
 } from './paramsBuilder';
 import { Transaction } from '@solana/web3.js';
+import { getWithdrawNftInstruction } from './instructions';
 import {
-  getDepositNftInstruction,
-  getInitDepositNftInstruction,
-  getInitReceiptMintInstruction,
-  getTransferReceiptNftToUserInstruction,
-  getWithdrawNftInstruction,
-} from './instructions';
-import {
-  DepositNftApiArgs,
   FetchAccountArgs,
   GroupedDepositNftApiArgs,
-  InitDepositNftApiArgs,
-  InitReceiptMintApiArgs,
   SendTxRequest,
-  TransferReceiptNftApiArgs,
   UniverseApiArgs,
   WithdrawNftApiArgs,
 } from './types/types';
@@ -67,88 +57,7 @@ const updateUniverse = async (args: UniverseApiArgs) => {
   }
 };
 
-const initReceiptMint = async (args: InitReceiptMintApiArgs) => {
-  try {
-    const program = getMetaBlocksProgram(args.connection, args.wallet);
-    const usersKey = args.wallet.publicKey;
-    const initReceiptMintInstruction = await getInitReceiptMintInstruction({
-      program: program,
-      usersKey: usersKey,
-      mintKey: args.mintKey,
-      universeKey: args.universeKey,
-    });
-    const transaction = new Transaction();
-    transaction.add(initReceiptMintInstruction);
-
-    const tx = await program.provider.send(transaction, []);
-    return tx;
-  } catch (e) {
-    throw e;
-  }
-};
-
-const initDepositNft = async (args: InitDepositNftApiArgs) => {
-  try {
-    const program = getMetaBlocksProgram(args.connection, args.wallet);
-    const usersKey = args.wallet.publicKey;
-    const initDepositNftInstruction = await getInitDepositNftInstruction({
-      program: program,
-      usersKey: usersKey,
-      mintKey: args.mintKey,
-      universeKey: args.universeKey,
-    });
-    const transaction = new Transaction();
-    transaction.add(initDepositNftInstruction);
-
-    return await program.provider.send(transaction, []);
-  } catch (e) {
-    throw e;
-  }
-};
-
-const depositNft = async (args: DepositNftApiArgs) => {
-  try {
-    const program = getMetaBlocksProgram(args.connection, args.wallet);
-    const usersKey = args.wallet.publicKey;
-    const depositNftInstruction = await getDepositNftInstruction({
-      program: program,
-      usersKey: usersKey,
-      mintKey: args.mintKey,
-      universeKey: args.universeKey,
-    });
-    const transaction = new Transaction();
-    transaction.add(depositNftInstruction);
-
-    return await program.provider.send(transaction, []);
-  } catch (e) {
-    throw e;
-  }
-};
-
-const transferReceiptNftToUser = async (args: TransferReceiptNftApiArgs) => {
-  try {
-    const program = getMetaBlocksProgram(args.connection, args.wallet);
-    const usersKey = args.wallet.publicKey;
-    const transferReceiptNftToUserInstruction =
-      await getTransferReceiptNftToUserInstruction({
-        program: program,
-        usersKey: usersKey,
-        mintKey: args.mintKey,
-        universeKey: args.universeKey,
-        url: args.url,
-        isReceiptMasterEdition: args.isReceiptMasterEdition,
-      });
-    const transaction = new Transaction();
-    transaction.add(transferReceiptNftToUserInstruction);
-
-    const tx = await program.provider.send(transaction, []);
-    return tx;
-  } catch (e) {
-    throw e;
-  }
-};
-
-const groupedDepositNft = async (args: GroupedDepositNftApiArgs) => {
+const depositNft = async (args: GroupedDepositNftApiArgs) => {
   try {
     const sendTxRequests: Array<SendTxRequest> = [];
 
@@ -257,11 +166,7 @@ const getAllUserNfts = async (args: FetchAccountArgs) => {
 export {
   createUniverse,
   updateUniverse,
-  initReceiptMint,
-  initDepositNft,
   depositNft,
-  transferReceiptNftToUser,
-  groupedDepositNft,
   withdrawNft,
   getAllUniverses,
   getAllUserNfts,
