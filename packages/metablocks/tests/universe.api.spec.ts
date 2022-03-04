ba570b0b5665e949963b7e4fd171806d4db41e3c
valekar <srinivas.valekar@gmail.com>
2022-03-04 21:20:13 +0000

    test: Add universe test cases

--- packages/metablocks/tests/universe.api.spec.ts
+++ packages/metablocks/tests/universe.api.spec.ts
@@ -1,36 +1,176 @@
-import { assert, expect } from 'chai';
+import { assert } from 'chai';
 import * as anchor from '@project-serum/anchor';
 import { Wallet } from '@project-serum/anchor';
 import { MetaBlocks } from '../src/types/meta_blocks';
-import { Connection, LAMPORTS_PER_SOL, SystemProgram } from '@solana/web3.js';
-import { createUniverse } from '../src/api';
+import {
+  Connection,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SystemProgram,
+} from '@solana/web3.js';
+import { createUniverse, updateUniverse } from '../src/api';
 import { UniverseApiArgs } from '../src';
-import NodeWallet, { addSols } from './utils';
+import NodeWallet, { addSols, addSolsIfEmpty, getTestWallet } from './utils';
 import { getMetaBlocksProgram } from '../src/factory';
+import { findUniverseAddress } from '../src/pda';
 
-describe('Create universe', () => {
+describe('Universe Test Cases', () => {
+  const testWallet = getTestWallet();
   const dummyKeypair = anchor.web3.Keypair.generate();
   const dummyWallet = new NodeWallet(dummyKeypair);
+
   const connection = new anchor.web3.Connection(
     'https://api.devnet.solana.com',
     'confirmed'
   );
-  const program = getMetaBlocksProgram(connection, dummyWallet);
-  program.simulate;
+  const program = getMetaBlocksProgram(connection, testWallet);
+  let universePda: [PublicKey, number] = [dummyKeypair.publicKey, 0];
 
   beforeAll(async () => {
-    await addSols(program.provider, dummyWallet.publicKey, LAMPORTS_PER_SOL);
+    await addSolsIfEmpty(program.provider, testWallet, 2 * LAMPORTS_PER_SOL);
+    await addSols(program.provider, dummyKeypair.publicKey, 10000000);
+    universePda = await findUniverseAddress(dummyWallet.publicKey);
   });
 
-  it('should pass', async () => {
-    const args: UniverseApiArgs = {
-      name: 'universe',
-      description: 'universe',
-      websiteUrl: 'universe',
+  /**
+   * Test create universe
+   */
+  it('should create universe', async () => {
+    const args = {
+      name: sampleUniverseData().name,
+      description: sampleUniverseData().description,
+      websiteUrl: sampleUniverseData().websiteUrl,
       connection: connection,
       wallet: dummyWallet,
     };
 
-    await createUniverse(args);
+    const tx = await createUniverse(args);
+
+    const universeData = await program.account.universe.fetch(universePda[0]);
+
+    assert.isOk(
+      universeData.authority.toString() === dummyWallet.publicKey.toString()
+    );
+    assert.isOk(
+      universeData.config.websiteUrl === sampleUniverseData().websiteUrl
+    );
+    assert.equal(
+      universeData.config.name,
+      sampleUniverseData().name,
+      'Universe Name should be equal'
+    );
+    assert.equal(
+      universeData.config.description,
+      sampleUniverseData().description,
+      'Universe description should be equal'
+    );
+  });
+
+  /**
+   * Test Update universe
+   */
+  it('should update universe ', async () => {
+    const beforeUniverseData = await program.account.universe.fetch(
+      universePda[0]
+    );
+
+    assert.isOk(
+      beforeUniverseData.authority.toString() ===
+        dummyWallet.publicKey.toString()
+    );
+    assert.isOk(
+      beforeUniverseData.config.websiteUrl === sampleUniverseData().websiteUrl
+    );
+    assert.equal(
+      beforeUniverseData.config.name,
+      sampleUniverseData().name,
+      'Universe Name should be equal'
+    );
+    assert.equal(
+      beforeUniverseData.config.description,
+      sampleUniverseData().description,
+      'Universe description should be equal'
+    );
+
+    const args = {
+      name: sampleUpdatedUniverseData().name,
+      description: sampleUpdatedUniverseData().description,
+      websiteUrl: sampleUpdatedUniverseData().websiteUrl,
+      connection: connection,
+      wallet: dummyWallet,
+    };
+
+    const tx = await updateUniverse(args);
+    const universeData = await program.account.universe.fetch(universePda[0]);
+    assert.isOk(
+      universeData.authority.toString() === dummyWallet.publicKey.toString()
+    );
+
+    assert.isOk(
+      universeData.authority.toString() === dummyWallet.publicKey.toString()
+    );
+    assert.isOk(
+      universeData.config.websiteUrl === sampleUpdatedUniverseData().websiteUrl
+    );
+    assert.equal(
+      universeData.config.name,
+      sampleUpdatedUniverseData().name,
+      'Universe Name should be equal'
+    );
+    assert.equal(
+      universeData.config.description,
+      sampleUpdatedUniverseData().description,
+      'Universe description should be equal'
+    );
+  });
+
+  /**
+   * Test failed update universe
+   */
+  it('should fail to update universe', async () => {
+    const args = {
+      name: sampleUniverseData().name,
+      description: sampleUniverseData().description,
+      websiteUrl: sampleUniverseData().websiteUrl,
+      connection: connection,
+      wallet: testWallet, // different wallet
+    };
+
+    await updateUniverse(args);
+
+    const universeData = await program.account.universe.fetch(universePda[0]);
+
+    assert.isOk(
+      universeData.authority.toString() === dummyWallet.publicKey.toString()
+    );
+    assert.isOk(
+      universeData.config.websiteUrl === sampleUpdatedUniverseData().websiteUrl
+    );
+    assert.equal(
+      universeData.config.name,
+      sampleUpdatedUniverseData().name,
+      'Universe Name should be equal'
+    );
+    assert.equal(
+      universeData.config.description,
+      sampleUpdatedUniverseData().description,
+      'Universe description should be equal'
+    );
   });
 });
+
+const sampleUniverseData = () => {
+  return {
+    name: 'universe',
+    description: 'description',
+    websiteUrl: 'http://dummylocalhost.com',
+  };
+};
+
+const sampleUpdatedUniverseData = () => {
+  return {
+    name: 'updatedUniverse',
+    description: 'updatedDescription',
+    websiteUrl: 'http://updatedDummylocalhost.com',
+  };
+};
--- packages/metablocks/tests/utils.ts
+++ packages/metablocks/tests/utils.ts
@@ -1,5 +1,10 @@
 import { Provider, Wallet } from '@project-serum/anchor';
-import { Keypair, PublicKey, Transaction } from '@solana/web3.js';
+import {
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  Transaction,
+} from '@solana/web3.js';
 import * as anchor from '@project-serum/anchor';
 
 export default class NodeWallet implements Wallet {
@@ -30,5 +35,28 @@ export const addSols = async (
   await provider.connection.confirmTransaction(
     await provider.connection.requestAirdrop(wallet, amount),
     'confirmed'
+  );
+};
+
+export const addSolsIfEmpty = async (
+  provider: Provider,
+  wallet: NodeWallet,
+  amount: number
+) => {
+  const balance = await provider.connection.getBalance(wallet.publicKey);
+  if (balance <= LAMPORTS_PER_SOL) {
+    await addSols(provider, wallet.publicKey, amount);
+  }
+};
+
+export const getTestWallet = () => {
+  return new NodeWallet(
+    Keypair.fromSecretKey(
+      Buffer.from(
+        JSON.parse(
+          require('fs').readFileSync('./tests/wallet/test-signer.json', 'utf-8')
+        )
+      )
+    )
   );
 };
